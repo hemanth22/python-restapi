f2862dd7729a9033ad41dd7bf95fb0bc84cb5a73
Hemanth B <hemanth22hemu@gmail.com>
2022-08-09 22:33:02 +0530

    Update restapitest2.py

--- restapitest2.py
+++ restapitest2.py
@@ -1,6 +1,14 @@
 import xml.etree.ElementTree as ET
 import requests
 
+
+def status_beautify(responsestatus_code):
+    if responsestatus_code == 200:
+        return 'OK'
+    else:
+        return 'ERROR'
+
+
 url = "http://www.dneonline.com/calculator.asmx"
 
 payload = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\r\n  <soap:Body>\r\n    <Add xmlns=\"http://tempuri.org/\">\r\n      <intA>3</intA>\r\n      <intB>4</intB>\r\n    </Add>\r\n  </soap:Body>\r\n</soap:Envelope>"
@@ -13,4 +21,4 @@ response = requests.request("POST", url, headers=headers, data=payload)
 tree = ET.fromstring(response.text)
 
 for xmlresponse in tree.findall('.//{*}AddResult'):
-    print('Connection: {0} , price: {1} '.format(response.status_code,xmlresponse.text))
+    print('Connection: {0} , price: {1} '.format(status_beautify(response.status_code),xmlresponse.text))
