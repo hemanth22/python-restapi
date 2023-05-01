8d13d5a4b605e5c4e03382c3ae3c87f744e49e99
Hemanth B <hemanth22hemu@gmail.com>
2023-05-01 19:45:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-uvicorn main:app --reload > vault_test.out 2>&1 & echo $! > python.pid
+uvicorn main:app --host 0.0.0.0 --port 8000 > vault_test.out 2>&1 & echo $! > python.pid
 echo "[FAST API] Service started"
